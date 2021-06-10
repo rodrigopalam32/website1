00bcf911bfb99a209c32db6513d1e6a27fe53695
rodrigopalam32 <83837643+rodrigopalam32@users.noreply.github.com>
2021-06-10 07:11:41 -0600

    Add files via upload

---
+++ script 2.js
@@ -0,0 +1,23 @@
+function monika() {
+	document.getElementById('monika').style.display = 'block';
+	document.getElementById('mc').style.display = 'block';
+	document.getElementById('m').style.display = 'none';
+}
+
+function mc() {
+	document.getElementById('monika').style.display = 'none';
+	document.getElementById('mc').style.display = 'none';
+	document.getElementById('monika2').style.display = 'block';
+}
+
+function yuri() {
+	document.getElementById('yuri').style.display = 'block';
+	document.getElementById('mc2').style.display = 'block';
+	document.getElementById('y').style.display = 'none';
+}
+
+function mc2() {
+	document.getElementById('yuri').style.display = 'none';
+	document.getElementById('mc2').style.display = 'none';
+	document.getElementById('yuri2').style.display = 'block';
+}
